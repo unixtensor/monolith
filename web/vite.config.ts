fb0c0f587dc211b804957468e897ed700affd780
rhpidfyre <brandon@rhpidfyre.io>
2026-03-18 20:37:12 -0400

    Remove web generated boilerplate and add our own boilerplate

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,11 +1,19 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import babel from "@rolldown/plugin-babel";
+import tailwindcss from "@tailwindcss/vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
-})
+	plugins: [
+		tailwindcss(),
+		react(),
+		babel({ presets: [reactCompilerPreset()] }),
+	],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+});
