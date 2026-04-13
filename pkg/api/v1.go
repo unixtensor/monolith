f8cb6e4887134e90bfdb19bb4b3f42090d913eb5
rhpidfyre <brandon@rhpidfyre.io>
2026-04-13 02:01:37 -0400

    Use `POST` instead on root for login requests

--- pkg/api/v1.go
+++ pkg/api/v1.go
@@ -16,17 +16,15 @@ type Config struct {
 func (c *Config) v1(api_root *gin.Engine) {
 	api_v1 := api_root.Group("/api/v1")
 	{
-		api_v1.POST("/login", func(ctx *gin.Context) { v1.Login(ctx, c.Token) })
-		{
-			api_v1.Use(VerifyToken(c.Token))
-			api_v1.GET("/", func(ctx *gin.Context) { ctx.Status(http.StatusOK) })
-			api_v1.GET("/games-servers", v1.Servers)
-			api_v1.GET(":placeId", v1.Connected)
-			api_v1.POST(":placeId/:jobId", v1.Connect)
-			api_v1.POST(":placeId/:jobId/disconnect", v1.Disconnect)
-			api_v1.POST(":placeId/:jobId/instance", v1.UploadInstance)
-			api_v1.GET(":placeId/:jobId/instance", v1.GetInstance)
-		}
+		api_v1.POST("/", func(ctx *gin.Context) { v1.Login(ctx, c.Token) })
+		api_v1.Use(VerifyToken(c.Token))
+		api_v1.GET("/", func(ctx *gin.Context) { ctx.Status(http.StatusOK) })
+		api_v1.GET("/games-servers", v1.Servers)
+		api_v1.GET(":placeId", v1.Connected)
+		api_v1.POST(":placeId/:jobId", v1.Connect)
+		api_v1.POST(":placeId/:jobId/disconnect", v1.Disconnect)
+		api_v1.POST(":placeId/:jobId/instance", v1.UploadInstance)
+		api_v1.GET(":placeId/:jobId/instance", v1.GetInstance)
 	}
 }
 
